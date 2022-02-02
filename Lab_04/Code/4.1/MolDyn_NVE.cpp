94f99ea35176c4dff64caece6ef36f7470bc835e
MatteoRobbiati <robbiati.matteo@gmail.com>
2022-02-02 18:43:42 +0100

    done ex 4.1 in Lab_04

---
+++ Lab_04/Code/4.1/MolDyn_NVE.cpp
@@ -0,0 +1,388 @@
+/****************************************************************
+*****************************************************************
+    _/    _/  _/_/_/  _/       Numerical Simulation Laboratory
+   _/_/  _/ _/       _/       Physics Department
+  _/  _/_/    _/    _/       Universita' degli Studi di Milano
+ _/    _/       _/ _/       Prof. D.E. Galli
+_/    _/  _/_/_/  _/_/_/_/ email: Davide.Galli@unimi.it
+*****************************************************************
+*****************************************************************/
+#include <stdlib.h>     // srand, rand: to generate random number
+#include <iostream>     // cin, cout: Standard Input/Output Streams Library
+#include <fstream>      // Stream class to both read and write from/to files.
+#include <string>
+#include <vector>
+#include <stdio.h>
+#include <cmath>        // rint, pow
+#include "MolDyn_NVE.h"
+
+using namespace std;
+
+int main(){
+  Equilibrate_system();
+  Input();             //Inizialization
+  int nconf = 1;
+  for(int istep=1; istep <= nstep; ++istep){
+     Move();           //Move particles with Verlet algorithm
+     if(istep%iprint == 0) cout << "Number of time-steps: " << istep << endl;
+     if(istep%10 == 0){
+        Measure();     //Properties measurement
+//        ConfXYZ(nconf);//Write actual configuration in XYZ format //Commented to avoid "filesystem full"!
+        nconf += 1;
+     }
+  }
+  ConfFinal();         //Write final configuration to restart
+
+  return 0;
+}
+
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~ INPUT ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+void Input(void){ //Prepare all stuff for the simulation
+  ifstream ReadInput,ReadConf, ReadOld;
+  ofstream SaveConf;
+  double ep, ek, pr, et, vir;
+
+  seed = 1;    //Set seed for random numbers
+  srand(seed); //Initialize random number generator
+  ReadInput.open("input.dat"); //Read input
+
+  ReadInput >> temp;
+  ReadInput >> npart;
+  ReadInput >> rho;
+  vol = (double)npart/rho;
+  box = pow(vol,1.0/3.0);
+  ReadInput >> rcut;
+  ReadInput >> delta;
+  ReadInput >> nstep;
+  ReadInput >> iprint;
+  ReadInput >> restart;
+
+  if(restart=="false"){
+  cout << "Classic Lennard-Jones fluid        " << endl;
+  cout << "Molecular dynamics simulation in NVE ensemble  " << endl << endl;
+  cout << "Interatomic potential v(r) = 4 * [(1/r)^12 - (1/r)^6]" << endl << endl;
+  cout << "The program uses Lennard-Jones units " << endl;
+  cout << "Number of particles = " << npart << endl;
+  cout << "Density of particles = " << rho << endl;
+  cout << "Volume of the simulation box = " << vol << endl;
+  cout << "Edge of the simulation box = " << box << endl;
+  cout << "The program integrates Newton equations with the Verlet method " << endl;
+  cout << "Time step = " << delta << endl;
+  cout << "Number of steps = " << nstep << endl << endl;
+}
+  ReadInput.close();
+
+//Prepare array for measurements
+  iv = 0; //Potential energy
+  ik = 1; //Kinetic energy
+  ie = 2; //Total energy
+  it = 3; //Temperature
+  n_props = 4; //Number of observables
+
+//Read r(t) positions
+
+  if(restart=="true"){
+    cout << "Read initial configuration from file config.0 " << endl << endl;
+    ReadConf.open("config.0");
+    for (int i=0; i<npart; ++i){
+      ReadConf >> x[i] >> y[i] >> z[i];
+      x[i] = x[i] * box;
+      y[i] = y[i] * box;
+      z[i] = z[i] * box;
+    }
+    ReadConf.close();
+
+    SaveConf.open("old.0", ios::out | ios::trunc);
+    //Prepare initial velocities
+    cout << "Restart is True: preparing first time configuration in the new old.0 file" << endl;
+    cout << "Prepare random velocities with center of mass velocity equal to zero " << endl << endl;
+    double sumv[3] = {0.0, 0.0, 0.0};
+    for (int i=0; i<npart; ++i){
+      vx[i] = rand()/double(RAND_MAX) - 0.5;
+      vy[i] = rand()/double(RAND_MAX) - 0.5;
+      vz[i] = rand()/double(RAND_MAX) - 0.5;
+
+      sumv[0] += vx[i];
+      sumv[1] += vy[i];
+      sumv[2] += vz[i];
+    }
+    for (int idim=0; idim<3; ++idim) sumv[idim] /= (double)npart;
+    double sumv2 = 0.0, fs;
+    for (int i=0; i<npart; ++i){
+      vx[i] = vx[i] - sumv[0];
+      vy[i] = vy[i] - sumv[1];
+      vz[i] = vz[i] - sumv[2];
+
+      sumv2 += vx[i]*vx[i] + vy[i]*vy[i] + vz[i]*vz[i];
+    }
+    sumv2 /= (double)npart;
+
+    fs = sqrt(3 * temp / sumv2);   // fs = velocity scale factor
+    for (int i=0; i<npart; ++i){
+      vx[i] *= fs;
+      vy[i] *= fs;
+      vz[i] *= fs;
+
+      xold[i] = Pbc(x[i] - vx[i] * delta);
+      yold[i] = Pbc(y[i] - vy[i] * delta);
+      zold[i] = Pbc(z[i] - vz[i] * delta);
+      SaveConf << xold[i]/box << "  " << yold[i]/box << "  " << zold[i]/box << endl;
+    }
+    SaveConf.close();
+
+  } else if (restart=="false"){
+      cout << "Restart is false: opening old.0 and using it for defining r(t-dt)" << endl;
+      ReadOld.open("old.final");
+      ReadConf.open("config.final");
+
+      for (int i=0; i<npart; ++i){
+        ReadOld  >> xold[i] >> yold[i] >> zold[i];
+        ReadConf >> x[i] >> y[i] >> z[i];
+        xold[i] = xold[i] * box;
+        yold[i] = yold[i] * box;
+        zold[i] = zold[i] * box;
+        x[i] = x[i]*box;
+        y[i] = y[i]*box;
+        z[i] = z[i]*box;
+      }
+      ReadConf.close();
+      ReadOld.close();
+  }
+  return;
+}
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~ THERMALIZATION PHASE ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+void Equilibrate_system(){
+
+  cout << "####################################################################" << endl;
+  cout << "Thermalization phase of the simulation." << endl;
+  cout << "Running 10000 steps that will be ignored at the end of this phase." << endl << endl;
+  cout << "####################################################################" << endl;
+
+  Input();
+  for(int i=0; i<5000; i++){
+    if((i+1)%1000==0){
+      cout << "Thermalization process is running, step " << i+1 << "/10000. Rescaling velocities." << endl;
+      rescale_velocities();
+    }
+    if(i%10==0) Measure();
+    Move();
+  }
+  set_restart("true","false");
+  cout << "This is the end of the thermalization phase, let's the simulation begin. " << endl;
+  cout << "####################################################################" << endl << endl;
+
+  ConfFinal();
+
+  return;
+}
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ FZ FOR REPLACING RESTART VALUE ~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+void set_restart(string current, string new_val){
+  string strReplace = current;
+  string strNew = new_val;
+  ifstream filein("input.dat");
+  ofstream fileout("input.temp");
+
+  while(!filein.eof()){
+    string temp;
+    filein >> temp;
+    if(temp==current) fileout << new_val << endl;
+    else fileout << temp << endl;
+  }
+  rename("input.temp", "input.dat");
+}
+
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ MOVE ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+void Move(void){ //Move particles with Verlet algorithm
+  double xnew, ynew, znew, fx[m_part], fy[m_part], fz[m_part];
+
+  for(int i=0; i<npart; ++i){ //Force acting on particle i
+    fx[i] = Force(i,0);
+    fy[i] = Force(i,1);
+    fz[i] = Force(i,2);
+  }
+
+  for(int i=0; i<npart; ++i){ //Verlet integration scheme
+
+    xnew = Pbc( 2.0 * x[i] - xold[i] + fx[i] * pow(delta,2) );
+    ynew = Pbc( 2.0 * y[i] - yold[i] + fy[i] * pow(delta,2) );
+    znew = Pbc( 2.0 * z[i] - zold[i] + fz[i] * pow(delta,2) );
+
+    vx[i] = Pbc(xnew - xold[i])/(2.0 * delta);
+    vy[i] = Pbc(ynew - yold[i])/(2.0 * delta);
+    vz[i] = Pbc(znew - zold[i])/(2.0 * delta);
+
+    xold[i] = x[i];
+    yold[i] = y[i];
+    zold[i] = z[i];
+
+    x[i] = xnew;
+    y[i] = ynew;
+    z[i] = znew;
+  }
+  return;
+}
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ RESCALING VELOCITIES ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+void rescale_velocities(){
+
+  double sumv2 = 0.0;
+  double fs=0.0;
+  for(int i=0; i<npart; i++){
+    vx[i] = Pbc(x[i] - xold[i])/(delta);
+    vy[i] = Pbc(y[i] - yold[i])/(delta);
+    vz[i] = Pbc(z[i] - zold[i])/(delta);
+    sumv2+=vx[i]*vx[i] + vy[i]*vy[i] + vz[i]*vz[i];
+  }
+  sumv2 /= (double)npart;
+
+  fs = sqrt(3 * temp / sumv2);   // fs = velocity scale factor
+  cout << "Scale factor is " << fs << endl;
+  for (int i=0; i<npart; ++i){
+    vx[i] *= fs;
+    vy[i] *= fs;
+    vz[i] *= fs;
+
+    xold[i] = Pbc(x[i] - vx[i] * delta);
+    yold[i] = Pbc(y[i] - vy[i] * delta);
+    zold[i] = Pbc(z[i] - vz[i] * delta);
+  }
+  return;
+}
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ FORCE ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+double Force(int ip, int idir){ //Compute forces as -Grad_ip V(r)
+  double f=0.0;
+  double dvec[3], dr;
+
+  for (int i=0; i<npart; ++i){
+    if(i != ip){
+      dvec[0] = Pbc( x[ip] - x[i] );  // distance ip-i in pbc
+      dvec[1] = Pbc( y[ip] - y[i] );
+      dvec[2] = Pbc( z[ip] - z[i] );
+
+      dr = dvec[0]*dvec[0] + dvec[1]*dvec[1] + dvec[2]*dvec[2];
+      dr = sqrt(dr);
+
+      if(dr < rcut){
+        f += dvec[idir] * (48.0/pow(dr,14) - 24.0/pow(dr,8)); // -Grad_ip V(r)
+      }
+    }
+  }
+
+  return f;
+}
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ MEASURE ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+void Measure(){ //Properties measurement
+  int bin;
+  double v, t, vij;
+  double dx, dy, dz, dr;
+  ofstream Epot, Ekin, Etot, Temp;
+
+  Epot.open("output_epot.dat",ios::app);
+  Ekin.open("output_ekin.dat",ios::app);
+  Temp.open("output_temp.dat",ios::app);
+  Etot.open("output_etot.dat",ios::app);
+
+  v = 0.0; //reset observables
+  t = 0.0;
+
+//cycle over pairs of particles
+  for (int i=0; i<npart-1; ++i){
+    for (int j=i+1; j<npart; ++j){
+
+     dx = Pbc( xold[i] - xold[j] ); // here I use old configurations [old = r(t)]
+     dy = Pbc( yold[i] - yold[j] ); // to be compatible with EKin which uses v(t)
+     dz = Pbc( zold[i] - zold[j] ); // => EPot should be computed with r(t)
+
+     dr = dx*dx + dy*dy + dz*dz;
+     dr = sqrt(dr);
+
+     if(dr < rcut){
+       vij = 4.0/pow(dr,12) - 4.0/pow(dr,6);
+
+//Potential energy
+       v += vij;
+     }
+    }
+  }
+
+//Kinetic energy
+  for (int i=0; i<npart; ++i) t += 0.5 * (vx[i]*vx[i] + vy[i]*vy[i] + vz[i]*vz[i]);
+
+    stima_pot = v/(double)npart; //Potential energy per particle
+    stima_kin = t/(double)npart; //Kinetic energy per particle
+    stima_temp = (2.0 / 3.0) * t/(double)npart; //Temperature
+    stima_etot = (t+v)/(double)npart; //Total energy per particle
+
+    Epot << stima_pot  << endl;
+    Ekin << stima_kin  << endl;
+    Temp << stima_temp << endl;
+    Etot << stima_etot << endl;
+
+    Epot.close();
+    Ekin.close();
+    Temp.close();
+    Etot.close();
+
+    return;
+}
+
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ WRITE FINAL CONFIGURATION ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ AND FINAL t-dt CONFIGURATION ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+void ConfFinal(void){ //Write final configuration
+  ofstream WriteConf, WriteOld;
+
+  cout << "Print final configuration to file config.final " << endl << endl;
+  WriteConf.open("config.final");
+  WriteOld.open("old.final", ios::out | ios::trunc);
+
+  for (int i=0; i<npart; ++i){
+    WriteConf << x[i]/box    << "   " <<  y[i]/box    << "   " << z[i]/box << endl;
+    WriteOld  << xold[i]/box << "   " <<  yold[i]/box << "   " << zold[i]/box << endl;
+  }
+  WriteConf.close();
+  WriteOld.close();
+  return;
+}
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ SAVE CONFIGUATION IN XYZ FORMAT ~~~~~~~~~~~~~~~~~~~~~~~~~
+
+void ConfXYZ(int nconf){ //Write configuration in .xyz format
+  ofstream WriteXYZ;
+
+  WriteXYZ.open("frames/config_" + to_string(nconf) + ".xyz");
+  WriteXYZ << npart << endl;
+  WriteXYZ << "This is only a comment!" << endl;
+  for (int i=0; i<npart; ++i){
+    WriteXYZ << "LJ  " << Pbc(x[i]) << "   " <<  Pbc(y[i]) << "   " << Pbc(z[i]) << endl;
+  }
+  WriteXYZ.close();
+}
+
+double Pbc(double r){  //Algorithm for periodic boundary conditions with side L=box
+    return r - box * rint(r/box);
+}
+/****************************************************************
+*****************************************************************
+    _/    _/  _/_/_/  _/       Numerical Simulation Laboratory
+   _/_/  _/ _/       _/       Physics Department
+  _/  _/_/    _/    _/       Universita' degli Studi di Milano
+ _/    _/       _/ _/       Prof. D.E. Galli
+_/    _/  _/_/_/  _/_/_/_/ email: Davide.Galli@unimi.it
+*****************************************************************
+*****************************************************************/
---
+++ Lab_04/Code/4.1/MolDyn_NVE.h
@@ -0,0 +1,55 @@
+/****************************************************************
+*****************************************************************
+    _/    _/  _/_/_/  _/       Numerical Simulation Laboratory
+   _/_/  _/ _/       _/       Physics Department
+  _/  _/_/    _/    _/       Universita' degli Studi di Milano
+ _/    _/       _/ _/       Prof. D.E. Galli
+_/    _/  _/_/_/  _/_/_/_/ email: Davide.Galli@unimi.it
+*****************************************************************
+*****************************************************************/
+//parameters, observables
+#include <string>
+using namespace std;
+
+const int m_props=4;
+int n_props;
+int iv,ik,it,ie;
+double stima_pot, stima_kin, stima_etot, stima_temp;
+
+// averages
+double acc,att;
+
+//configuration
+const int m_part=108;
+double x[m_part],y[m_part],z[m_part],xold[m_part],yold[m_part],zold[m_part];
+double vx[m_part],vy[m_part],vz[m_part];
+
+// thermodynamical state
+int npart;
+double energy,temp,vol,rho,box,rcut;
+string restart;    // useful for restarting with r(t-dt) and r(t)
+
+// simulation
+int nstep, iprint, seed;
+double delta;
+
+//functions
+void Equilibrate_system();
+void set_restart(string, string);
+void rescale_velocities();
+void Input(void);
+void Move(void);
+void ConfFinal(void);
+void ConfXYZ(int);
+void Measure(void);
+double Force(int, int);
+double Pbc(double);
+/****************************************************************
+*****************************************************************
+    _/    _/  _/_/_/  _/       Numerical Simulation Laboratory
+   _/_/  _/ _/       _/       Physics Department
+  _/  _/_/    _/    _/       Universita' degli Studi di Milano
+ _/    _/       _/ _/       Prof. D.E. Galli
+_/    _/  _/_/_/  _/_/_/_/ email: Davide.Galli@unimi.it
+*****************************************************************
+*****************************************************************/
