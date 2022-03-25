2c29aa4b76e9d90e1e55fe98c5ad0ddb41a5df5b
MatteoRobbiati <robbiati.matteo@gmail.com>
2022-03-25 12:43:26 +0100

    check 25 mar 13:00

--- Lab_10/Code/10.1/main.cpp
+++ Lab_10/Code/10.1/main.cpp
@@ -15,16 +15,16 @@ int main(){
 
   Random rnd;
   rnd.Init();
-  rnd.generate_cities_on_circle(1, N, circle_circuit);
+  //rnd.generate_cities_on_circle(1, N, circle_circuit);
   rnd.generate_cities_in_square(1, N, square_circuit);
 
-  Salesman *Jim_on_circle = new Salesman(N, circle_circuit, rnd);
+  /*Salesman *Jim_on_circle = new Salesman(N, circle_circuit, rnd);
   cout << "Jim is now working on the cities displaced on the circle! " << endl;
   Jim_on_circle->show_chromo();
   Jim_on_circle->save_chromo("../../Results/circle_ini.dat", true);
   Jim_on_circle->simulated_annealing(1., 500, 500, 100, "../../Results/anneal_circle.dat");
   Jim_on_circle->show_chromo();
-  Jim_on_circle->save_chromo("../../Results/circle_fin.dat", true);
+  Jim_on_circle->save_chromo("../../Results/circle_fin.dat", true);*/
 
   Salesman *Jim_in_square = new Salesman(N, square_circuit, rnd);
   cout << "Jim is now working on the cities displaced in the square! " << endl;
