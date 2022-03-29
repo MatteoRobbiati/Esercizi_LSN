bb4c23d87f98dc6f77964a2cb392c4e516070bc6
MatteoRobbiati <robbiati.matteo@gmail.com>
2022-03-29 15:40:10 +0200

    JN check 29 mar

--- Lab_09/Code/9.1/main.cpp
+++ Lab_09/Code/9.1/main.cpp
@@ -16,15 +16,15 @@ int main(){
   string square_circuit = "../../Results/square_circuit.dat";
   Random rnd;
   rnd.Init();
-  rnd.generate_cities_on_circle(1, N, circle_circuit);
-  rnd.generate_cities_on_circle(1, 100, big_circle_cir);
+  //rnd.generate_cities_on_circle(1, N, circle_circuit);
+  //rnd.generate_cities_on_circle(1, 100, big_circle_cir);
   rnd.generate_cities_in_square(1, N, square_circuit);
 
-  Salesman *Jim_on_circle = new Salesman(N, M, circle_circuit, rnd);
+//  Salesman *Jim_on_circle = new Salesman(N, M, circle_circuit, rnd);
   Salesman *Jim_in_square = new Salesman(N, M, square_circuit, rnd);
-  Salesman *Jim_on_CIRCLE = new Salesman(100, M, big_circle_cir, rnd);
+//  Salesman *Jim_on_CIRCLE = new Salesman(100, M, big_circle_cir, rnd);
 
-  cout << "Jim is now working on the cities displaced on the circle! " << endl;
+/*  cout << "Jim is now working on the cities displaced on the circle! " << endl;
 
   ofstream out;
   out.open("../../Results/hist.dat");
@@ -32,25 +32,25 @@ int main(){
 
   Jim_on_circle->show_best_chromo();
   Jim_on_circle->save_chromo(0,"../../Results/circle_initial.dat", true);
-  Jim_on_circle->run(300);
+  Jim_on_circle->run(1000);
   Jim_on_circle->save_chromo(0,"../../Results/circle_final.dat", true);
-  Jim_on_circle->show_best_chromo();
+  Jim_on_circle->show_best_chromo();*/
 
   cout << endl << "Jim is now working into the square!" << endl;
 
   Jim_in_square->show_best_chromo();
   Jim_in_square->save_chromo(0,"../../Results/square_initial.dat", true);
-  Jim_in_square->run(300);
+  Jim_in_square->run(1000);
   Jim_in_square->save_chromo(0,"../../Results/square_final.dat", true);
   Jim_in_square->show_best_chromo();
 
-  cout << endl << "Jim is now working on the cities displaced on the BIG circle! " << endl;
+  /*cout << endl << "Jim is now working on the cities displaced on the BIG circle! " << endl;
 
   Jim_on_CIRCLE->show_best_chromo();
   Jim_on_CIRCLE->save_chromo(0,"../../Results/CIRCLE_initial.dat", true);
   Jim_on_CIRCLE->run(8000);
   Jim_on_CIRCLE->save_chromo(0,"../../Results/CIRCLE_final.dat", true);
-  Jim_on_CIRCLE->show_best_chromo();
+  Jim_on_CIRCLE->show_best_chromo();*/
 
   return 0;
 }
